289dc402de98fc00d51bfe747e0eeeafaac480ab
Shadow <kcoudray2001@gmail.com>
2020-11-09 15:09:29 +0100

    Add paramètres prédateur + objets proie done

--- src/com/company/Predator.java
+++ src/com/company/Predator.java
@@ -1,4 +1,30 @@
 package com.company;
 
 public class Predator {
+    int AgeNow;
+    int AgeMax;
+    static final int AgeMaxAll = 40;
+
+    int NbLifePointsNow;
+    int NbLifePointsMax;
+    static final int NbLifePointsMaxAll = 120;
+
+    Smelling smelling;
+    Vision vision;
+
+    public void move(){
+
+    }
+    public void hunt(Prey prey){
+
+    }
+    public void mate(Predator predator){
+
+    }
+    public void eat(Prey prey){
+
+    }
+    public void drink(WaterSpot waterSpot){
+
+    }
 }
--- src/com/company/Prey.java
+++ src/com/company/Prey.java
@@ -1,4 +1,30 @@
 package com.company;
 
 public class Prey {
+    int AgeNow;
+    int AgeMax;
+    static final int AgeMaxAll = 30;
+
+    int NbLifePointsNow;
+    int NbLifePointsMax;
+    static final int NbLifePointsMaxAll = 70;
+
+    Hearing hearing;
+    Vision vision;
+
+    public void move(){
+
+    }
+    public void flee(Predator predator){
+
+    }
+    public void mate(Prey prey){
+
+    }
+    public void eat(Plant plant){
+
+    }
+    public void drink(WaterSpot waterSpot){
+
+    }
 }
